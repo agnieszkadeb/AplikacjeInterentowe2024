4883f24107b846ec95db48d3c2719279ed3eaac8
LAPTOP-3O014AS5\Agusia <aga.debowiec@hotmail.com>
2024-05-26 11:55:48 +0200

    Zajęcia 6 -zmieniona struktura

--- config.php
+++ config.php
@@ -1,23 +1,26 @@
 <?php
 
 //wcześniejsze dane
-//define('_SERVER_NAME', 'localhost');
-//define('_SERVER_URL', 'http://'._SERVER_NAME);
-//define('_APP_ROOT', '/Projekt1');
-//define('_APP_URL', _SERVER_URL._APP_ROOT);
-//define("_ROOT_PATH", dirname(__FILE__));
+//require_once 'Config.class.php';
+//
+//$conf = new Config();
+//
+//$conf->root_path = dirname(__FILE__);
+//$conf->server_name = 'localhost:80';
+//$conf->server_url = 'http://'.$conf->server_name;
+//$conf->app_root = '/Projekt1';
+//$conf->app_url = $conf->server_url.$conf->app_root;
 
 
-
-require_once 'Config.class.php';
-
-$conf = new Config();
-
-$conf->root_path = dirname(__FILE__);
+//konfiguracja
 $conf->server_name = 'localhost:80';
 $conf->server_url = 'http://'.$conf->server_name;
 $conf->app_root = '/Projekt1';
-$conf->app_url = $conf->server_url.$conf->app_root;
+$conf->action_root = $conf->app_root.'/ctrl.php?action=';
 
+//wartości wygenerowane na podstawie powyższych
+$conf->action_url = $conf->server_url.$conf->action_root;
+$conf->app_url = $conf->server_url.$conf->app_root;
+$conf->root_path = dirname(__FILE__);
 
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__).'/config.php';
+require_once dirname(__FILE__).'/init.php';
 
 //przekierowanie (redirect) przeglądarki klienta -> header("Location: ".$conf->root_path."/app/calc.php"); -> przekazanie ("forward") żądania do następnego pliku 
-include $conf->root_path.'/app/calc.php';
+include $conf->root_path.'/ctrl.php';
